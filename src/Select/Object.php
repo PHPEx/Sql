86846467f858c6e0b820251a421b2b6b4c43138b
Igor C. de Paula <principe.borodin@gmail.com>
2015-05-08 16:05:19 -0300

    Add SubQuery on Select\Object

--- src/Select/Object.php
+++ src/Select/Object.php
@@ -5,7 +5,7 @@ namespace Sql\Select;
 use Sql\Select\Parameters\ParametersInterface; 
 use Sql\AbstractSql;
 
-final class Object extends AbstractSQL {
+final class Object extends AbstractSQL implements ParametersInterface{
 
     protected function _setData($fields, $values, $index = null) {
         
@@ -13,11 +13,15 @@ final class Object extends AbstractSQL {
 
     public function where(ParametersInterface $parameters) {
         if ($this->getQuery() == null) {
-            $this->_sql = "SELECT {$this->_fieldsTable} FROM {$this->_getTable()} WHERE {$parameters->interpret()}";
+            $this->_sql = "SELECT {$this->_fieldsTable} FROM {$this->_getTable()} WHERE ({$parameters->interpret()})";
         } else {
             $this->_sql .= $field;
         }
         return $this;
+    }
+
+    public function interpret() {
+        return $this->getQuery();
     }
 
 }
