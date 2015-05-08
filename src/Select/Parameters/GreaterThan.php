05b4741b87d1078d16012574277434cba3388f2c
Igor C. de Paula <principe.borodin@gmail.com>
2015-05-08 15:12:37 -0300

    Add GraterThan parameter

---
+++ src/Select/Parameters/GreaterThan.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Sql\Select\Parameters;
+
+use Sql\Select\Parameters\ParametersInterface;
+
+class GreaterThan implements ParametersInterface{
+    
+    private $value, $field;
+    
+    public function __construct(Field $field, Value $value) {
+            $this->value = $value;
+            $this->field = $field;
+    }
+
+    public function interpret() {        
+        $sql = " {$this->field->interpret()} >= {$this->value->interpret()}";
+        return $sql;
+    }
+}
