76a9ecd722dc7ac75aeb84f63a6c48dfa3ac6518
Igor C. de Paula <principe.borodin@gmail.com>
2015-05-08 16:33:45 -0300

    Add phpdoc to Equals, tests enviroment

--- src/Select/Parameters/Equals.php
+++ src/Select/Parameters/Equals.php
@@ -4,13 +4,32 @@ namespace Sql\Select\Parameters;
 
 class Equals implements ParametersInterface{
     
-    private $field,$value;
+    /**
+     *
+     * @var Field 
+     */
+    private $field;
     
+    /**
+     *
+     * @var Value 
+     */
+    private $value;
+    
+    /**
+     * Add Field and Value
+     * @param \Sql\Select\Parameters\Field $field Field of query
+     * @param \Sql\Select\Parameters\Value $value Value of field
+     */
     public function __construct(Field $field, Value $value) {
         $this->field = $field;                 
         $this->value = $value;                 
     }
 
+    /**
+     * 
+     * @return string
+     */
     public function interpret() {
         return "{$this->field->interpret()} = '{$this->value->interpret()}'";
     }
---
+++ tests/SqlTest/ObjectSelectTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace SqlTest;
+
+use Sql\Select\Object as Select;
+
+class ObjectSelectTest extends \PHPUnit_Framework_TestCase {
+   
+    private $object;
+    public function setUp()
+    {
+        $this->object = new Select();
+    }
+    
+    public function testTrue(){
+        $this->assertTrue(true);
+    }
+    
+    public function tearDown(){
+        $this->object = null;
+    }
+}
