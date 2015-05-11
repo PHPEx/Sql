b7cad69d5d7871b0ae7b4b20ad4ad9aa1d371589
Igor C. de Paula <principe.borodin@gmail.com>
2015-05-11 13:51:39 -0300

    Add OR Condition test

--- tests/SqlTest/ObjectSelectTest.php
+++ tests/SqlTest/ObjectSelectTest.php
@@ -6,6 +6,7 @@ use Sql\Select\Parameters\Field;
 use Sql\Select\Parameters\Equals;
 use Sql\Select\Parameters\Value;
 use Sql\Select\Parameters\Condition\AndCondition;
+use Sql\Select\Parameters\Condition\OrCondition;
 
 class ObjectSelectTest extends \PHPUnit_Framework_TestCase {
    
@@ -20,11 +21,13 @@ class ObjectSelectTest extends \PHPUnit_Framework_TestCase {
        $sel->setTable('users')->where();
        $this->assertEquals('SELECT * FROM users',$sel->getQuery());
     }
+    
     public function testEqualsParams(){
        $sel = new Select();
        $sel->setTable('users')->where(new Equals(new Field('id'),new Value(10)));
        $this->assertEquals("SELECT * FROM users WHERE (id = '10')",$sel->getQuery());
     }
+    
     public function testAndConditionAndEqualsParams(){
        $sel = new Select();
        $sel->setTable('users')->where(
@@ -34,6 +37,17 @@ class ObjectSelectTest extends \PHPUnit_Framework_TestCase {
                        )
                );
        $this->assertEquals("SELECT * FROM users WHERE (username = 'borodin' AND password = 'pass')",$sel->getQuery());
+    }
+    
+    public function testOrConditionAndEqualsParams(){
+       $sel = new Select();
+       $sel->setTable('users')->where(
+               new OrCondition(
+                       new Equals(new Field('username'),new Value('borodin')),
+                       new Equals(new Field('password'),new Value('pass'))
+                       )
+               );
+       $this->assertEquals("SELECT * FROM users WHERE (username = 'borodin' OR password = 'pass')",$sel->getQuery());
     }
     
     public function tearDown(){
