a5390b2ff5899bc7f75d425341cb90800e6f6e53
Igor C. de Paula <principe.borodin@gmail.com>
2015-05-11 13:32:21 -0300

    Test Equals params

--- tests/SqlTest/ObjectSelectTest.php
+++ tests/SqlTest/ObjectSelectTest.php
@@ -2,6 +2,9 @@
 namespace SqlTest;
 
 use Sql\Select\ObjectSelect as Select;
+use Sql\Select\Parameters\Field;
+use Sql\Select\Parameters\Equals;
+use Sql\Select\Parameters\Value;
 
 class ObjectSelectTest extends \PHPUnit_Framework_TestCase {
    
@@ -15,6 +18,11 @@ class ObjectSelectTest extends \PHPUnit_Framework_TestCase {
        $sel = new Select();
        $sel->setTable('users')->where();
        $this->assertEquals('SELECT * FROM users',$sel->getQuery());
+    }
+    public function testEqualsParams(){
+       $sel = new Select();
+       $sel->setTable('users')->where(new Equals(new Field('id'),new Value(10)));
+       $this->assertEquals('SELECT * FROM users WHERE id=10',$sel->getQuery());
     }
     
     public function tearDown(){
