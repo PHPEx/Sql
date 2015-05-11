583cd61ab96fe5c284b3d1e1986bcb8710c6c434
Igor C. de Paula <principe.borodin@gmail.com>
2015-05-11 13:38:20 -0300

    Fixed second test

--- src/Select/ObjectSelect.php
+++ src/Select/ObjectSelect.php
@@ -5,17 +5,19 @@ namespace Sql\Select;
 use Sql\Select\Parameters\ParametersInterface; 
 use Sql\AbstractSql;
 
-final class ObjectSelect extends AbstractSQL implements ParametersInterface{
+final class ObjectSelect extends AbstractSql implements ParametersInterface{
 
     protected function _setData($fields, $values, $index = null) {
         
     }
 
     public function where(ParametersInterface $parameters = null) {
-        if ($this->getQuery() == null) {
+        if ($this->getQuery() == null && $parameters == null) {
             $this->_sql = "SELECT {$this->_fieldsTable} FROM {$this->_getTable()}";
-        } else {
-            $this->_sql .= " WHERE ({$parameters->interpret()})";
+        } 
+        
+        if($this->getQuery() == null && $parameters != null){
+            $this->_sql .= "SELECT {$this->_fieldsTable} FROM {$this->_getTable()} WHERE ({$parameters->interpret()})";
         }
         return $this;
     }
